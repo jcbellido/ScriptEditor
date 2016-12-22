e06397b9a96742cde365f5e27f9e77a9976760be
Luis <Luis@PC-LUIS>
2016-12-22 01:02:04 +0100

    First creation of the project, VS configured, Unit Test infrastructure created.

---
+++ ScriptEditor/Model/account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptEditor.Model
+{
+    public class Account
+    {
+        protected int m_value;
+
+        public Account(int value)
+        {
+            m_value = value;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return m_value;
+            }
+            set
+            {
+                m_value = 2 * value;
+            }
+        }
+    }
+}
---
+++ ScriptEditorUnitTest_noWin81/testAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ScriptEditor.Model;
+
+namespace ScriptEditorUnitTest_noWin81
+{
+    [TestClass]
+    public class testAccount
+    {
+        [TestMethod]
+        public void TestAccountObject()
+        {
+            Account a = new Account(1);
+            Assert.IsTrue(a.Value == 1, "Failed initialization");
+            a.Value = 2;
+            Assert.IsTrue(a.Value == 2, "Failed set property");
+        }
+    }
+}
